128444c97258ee612d4cbf144e863d49c3111512
RoundRonin <roundronin8@gmail.com>
2024-03-30 00:22:54 +0300

    Changes

--- .zshrc
+++ .zshrc
@@ -118,3 +118,8 @@ eval $(thefuck --alias)
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 . "$HOME/.cargo/env"
+# [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- install.sh
+++ install.sh
@@ -10,6 +10,10 @@ apt install zsh -y
 ## OH MY ZSH
 `sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"` -y
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 ## NVIM
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
@@ -17,9 +21,16 @@ sudo tar -C /opt -xzf nvim-linux64.tar.gz
 export PATH="$PATH:/opt/nvim-linux64/bin"
 rm -rf nvim-linux64.tar.gz
 
+## Keychain
+sudo apt install keychain
+
 ## SSH keys
 
 ## TMUX
+sudo apt install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+tmux source ~/.tmux.conf
 
 ## fd find
 sudo apt install fd-find
@@ -31,3 +42,29 @@ curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | 
 ## Fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+## Htop
+sudo apt install htop
+
+## TheFuck
+sudo apt update
+sudo apt install python3-dev python3-pip python3-setuptools
+pip3 install thefuck --user
+
+# Compilers
+#
+## C
+
+sudo apt install clang -y
+
+## Rust
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+
+## JS
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+source ~/.zshrc
+
+nvm install --ltc
